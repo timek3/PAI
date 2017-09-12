66a9191302b31b9f027fe12b4db6bd059b913b52
timek3 <timek11298@wp.pl>
2017-09-12 12:13:36 +0200

    pro elo informatyk

--- 09.11.17/js/funkcje.js
+++ 09.11.17/js/funkcje.js
@@ -1,13 +1,29 @@
-function witaj (imie){
+/*function witaj (imie){
     document.write("Witaj " + imie);
     
 }
 var name = 'Janusz <br>';
-witaj(name);
-var a = prompt("podaj a");
+witaj(name);*/
+// funkcja pola
+/*var a = prompt("podaj a");
 var b = prompt("podaj b");
 function pole(a,b) {
     document.write("Pole jest równe " + a*b);
     
 }
-pole (a,b);
+pole (a,b);*/
+
+/*function poleObjetosc(szerokosc, dlugosc, wysokosc){
+    var pole = szerokosc * dlugosc;
+    var objetosc = pole * wysokosc;
+    var wynik = [pole,objetosc];
+    return wynik;
+}
+var a = prompt('Podaj szerokosc');
+var b = prompt('Podaj długość');
+var c = prompt('Podaj wysokość');
+var x = prompt('Podaj wartość','0 - pole, 1 objętość');
+var poleProstokata = poleObjetosc(a,b,c)[x];
+console.log(poleProstokata);*/
+
+//zadanie napisać skrypt który oblicza obwód, pole koła, oraz objętość stożka. Dane użytkownik podaje z klawiatury, wykorzystaj obiekt math. Użytkownik wybiera w radio buton co chce obliczyć. dane wyświetl w bloku strony.
