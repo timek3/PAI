598ddc0eee576def3d963209b23f3a374e799ad6
timek3 <31842105+timek3@users.noreply.github.com>
2017-09-12 11:46:56 +0200

    Add files via upload

---
+++ 09.11.17/js/funkcje.js
@@ -0,0 +1,13 @@
+function witaj (imie){
+    document.write("Witaj " + imie);
+    
+}
+var name = 'Janusz <br>';
+witaj(name);
+var a = prompt("podaj a");
+var b = prompt("podaj b");
+function pole(a,b) {
+    document.write("Pole jest równe " + a*b);
+    
+}
+pole (a,b);
